4c22d6ecceafdb805601eaf4e1565026c508cb2d
SebinDuke <sebindukestephan@gmail.com>
2017-02-01 23:50:50 -0500

    adding layout

--- TheTalkSquare/wsgi.py
+++ TheTalkSquare/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HackCSI project.
+WSGI config for TheTalkSquare project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HackCSI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TheTalkSquare.settings")
 
 application = get_wsgi_application()
