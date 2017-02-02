2107f61fe63920f621edb723e2672f909decf533
SebinDuke <sebindukestephan@gmail.com>
2017-02-02 00:23:48 -0500

    models.py

--- main/models.py
+++ main/models.py
@@ -7,17 +7,17 @@ class Users(models.Model):
     email = models.CharField(max_length=120)
     pwd= models.CharField(max_length=80)
     no_of_topics=models.IntegerField(default=0)
-    
+    type=models.IntegerField(default=0) #simple=0 org=1
     def __str__(self):
         return self.user_name
 
 
 class Topic(models.Model):
-    user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    tags = models.ForeignKey(Tag, on_delete = models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     topic_text = models.CharField(max_length=250)
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
-
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
@@ -25,12 +25,20 @@ class Topic(models.Model):
     def __str__(self):
         return self.topic_text
 
+
+class Tag(models.Model):
+    tag_name= models.CharField(max_length=50)
+    tag_desc=models.CharField(max_length=500,default="")
+
+    def __str__(self):
+        return self.tag_name
+
 class Opinion(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
     opinion_text=models.CharField(max_length=500)
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
-
+    foragainst=models.IntegerField(default=0) #for=1 against=0
     def __str__(self):
         return self.user.user_name
